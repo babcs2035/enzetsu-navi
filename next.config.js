116d8916d081af76d81024215de4743e80bc41d9
babcs2035 <babcs2035@gmail.com>
2026-01-28 15:59:26 +0900

    👷 optimize Dockerfile for multi-stage build, enhance Prisma and Playwright setup, and configure Next.js server actions.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
   basePath: "/enzetsu-navi",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["ktak.dev", "localhost:3000", "localhost:3300"],
+    },
+  },
 };
 
 module.exports = nextConfig;
